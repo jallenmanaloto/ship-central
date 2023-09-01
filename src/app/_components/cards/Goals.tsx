a9f7a0431bc2816a98db8869c338e0c059b699e7
jallenmanaloto <84300846+jallenmanaloto@users.noreply.github.com>
2023-09-01 11:11:27 +0800

    Fixed progress formatting

--- src/app/_components/cards/Goals.tsx
+++ src/app/_components/cards/Goals.tsx
@@ -5,9 +5,9 @@ import { vesselProgress } from '@/app/faker/data/goals'
 const Goals = () => {
 	return (
 		<div className="block rounded-lg bg-white p-6 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700">
-			<h5 className="mb-2 pb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
+			<h2 className="mb-2 pb-2 text-xl font-semibold leading-tight md:text-4xl text-neutral-800 dark:text-neutral-50">
 				Vessel Progress
-			</h5>
+			</h2>
 			{vesselProgress.map((vessel) => {
 				return (
 					<>
--- src/app/_components/progress/index.tsx
+++ src/app/_components/progress/index.tsx
@@ -8,11 +8,11 @@ const Progress = ({
 	percentage: string
 }) => {
 	return (
-		<div className="flex justify-start items-center py-3">
-			<h5 className="text-xs min-w-[80px] max-w-[80px] pr-3">{name}</h5>
-			<div className="h-1 w-44 bg-neutral-200 dark:bg-neutral-600">
+		<div className="flex justify-between items-center py-3">
+			<h5 className="text-sm min-w-[100px] max-w-[100px] pr-3">{name}</h5>
+			<div className="h-2 w-full bg-neutral-200 dark:bg-neutral-600">
 				<div
-					className="h-1 bg-sky-800"
+					className="h-2 bg-sky-800"
 					style={{ width: `${percentage}` }}></div>
 			</div>
 			<h5 className="text-xs pl-5">{percentage}</h5>
