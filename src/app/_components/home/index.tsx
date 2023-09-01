58c4cd593127600714168220d7ee104c365b72ae
jallenmanaloto <84300846+jallenmanaloto@users.noreply.github.com>
2023-09-01 20:19:27 +0800

    Fixed scrolling overflow

--- src/app/_components/home/index.tsx
+++ src/app/_components/home/index.tsx
@@ -4,7 +4,7 @@ import Forecast from '../forecast'
 
 const Home = () => {
 	return (
-		<div className="h-full pb-10">
+		<div className="h-full pb-10 overflow-y-auto">
 			<div className="w-full h-[268px] bg-sky-800" />
 			<div className="w-full t-0 grid grid-cols-1 -mt-20 md:grid-cols-2 gap-7 px-10">
 				<Goals />
