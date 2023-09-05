1980f28e253139ac22e8256056bc09e5ca971f05
jallenmanaloto <84300846+jallenmanaloto@users.noreply.github.com>
2023-09-05 21:00:33 +0800

    Updated nav icons

--- src/app/_components/sidebar/DefaultNav.tsx
+++ src/app/_components/sidebar/DefaultNav.tsx
@@ -20,6 +20,11 @@ const DefaultNav = () => {
 						Ship Central
 					</h1>
 				</div>
+				<div className="flex items-center py-3 px-6">
+					<h2 className="text-sm text-category font-semibold tracking-category">
+						REPORTS
+					</h2>
+				</div>
 				<Link href="/dashboard">
 					<div className="flex items-center py-2 px-6 group">
 						<svg
@@ -45,15 +50,10 @@ const DefaultNav = () => {
 									? 'text-dashboard'
 									: 'text-categoryInactive'
 							} ml-1 font-medium group-hover:text-dashboard`}>
-							Dashboard
+							Overview
 						</h2>
 					</div>
 				</Link>
-				<div className="flex items-center py-3 px-6">
-					<h2 className="text-sm text-category font-semibold tracking-category">
-						REPORTS
-					</h2>
-				</div>
 				<Link href="/dashboard/daily-loading">
 					<div className="flex items-center py-3 px-6 cursor-pointer group">
 						<svg
@@ -83,27 +83,80 @@ const DefaultNav = () => {
 						</h2>
 					</div>
 				</Link>
-				<div className="flex items-center py-3 px-6 cursor-pointer ">
-					<PiBoat style={{ color: '#919eab' }} />
-					<h2 className="text-categoryInactive w-full text-md ml-2 font-medium hover:text-white">
-						LCT Loading
-					</h2>
-				</div>
-				<div className="flex items-center py-3 px-6 cursor-pointer ">
-					<SiSimpleanalytics style={{ color: '#919eab' }} />
-					<h2 className="text-categoryInactive w-full text-md ml-2 font-medium hover:text-white">
-						Projects
-					</h2>
-				</div>
 				<div className="flex items-center py-3 px-6">
 					<h2 className="text-sm text-category font-semibold tracking-category">
-						REQUESTS
+						SHIPS
 					</h2>
 				</div>
 				<div className="flex items-center py-3 px-6 cursor-pointer ">
-					<RiCoinsFill style={{ color: '#919eab' }} />
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						strokeWidth={1.5}
+						stroke="currentColor"
+						className={`w-5 h-5 ${
+							pathname === '/dashboard/projects'
+								? 'stroke-dashboard'
+								: 'stroke-categoryInactive'
+						} group-hover:stroke-dashboard`}>
+						<path
+							strokeLinecap="round"
+							strokeLinejoin="round"
+							d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25zM6.75 12h.008v.008H6.75V12zm0 3h.008v.008H6.75V15zm0 3h.008v.008H6.75V18z"
+						/>
+					</svg>
 					<h2 className="text-categoryInactive w-full text-md ml-2 font-medium hover:text-white">
-						Budget
+						My Projects
+					</h2>
+				</div>
+				<div className="flex items-center py-3 px-6 cursor-pointer ">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						strokeWidth={1.5}
+						stroke="currentColor"
+						className={`w-5 h-5 ${
+							pathname === '/dashboard/daily-loading'
+								? 'stroke-dashboard'
+								: 'stroke-categoryInactive'
+						} group-hover:stroke-dashboard`}>
+						<path
+							strokeLinecap="round"
+							strokeLinejoin="round"
+							d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.324.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 011.37.49l1.296 2.247a1.125 1.125 0 01-.26 1.431l-1.003.827c-.293.24-.438.613-.431.992a6.759 6.759 0 010 .255c-.007.378.138.75.43.99l1.005.828c.424.35.534.954.26 1.43l-1.298 2.247a1.125 1.125 0 01-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.57 6.57 0 01-.22.128c-.331.183-.581.495-.644.869l-.213 1.28c-.09.543-.56.941-1.11.941h-2.594c-.55 0-1.02-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 01-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 01-1.369-.49l-1.297-2.247a1.125 1.125 0 01.26-1.431l1.004-.827c.292-.24.437-.613.43-.992a6.932 6.932 0 010-.255c.007-.378-.138-.75-.43-.99l-1.004-.828a1.125 1.125 0 01-.26-1.43l1.297-2.247a1.125 1.125 0 011.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.087.22-.128.332-.183.582-.495.644-.869l.214-1.281z"
+						/>
+						<path
+							strokeLinecap="round"
+							strokeLinejoin="round"
+							d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+						/>
+					</svg>
+					<h2 className="text-categoryInactive w-full text-md ml-2 font-medium hover:text-white">
+						Vessels
+					</h2>
+				</div>
+				<div className="flex items-center py-3 px-6 cursor-pointer ">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						strokeWidth={1.5}
+						stroke="currentColor"
+						className={`w-5 h-5 ${
+							pathname === '/dashboard/vessels'
+								? 'stroke-dashboard'
+								: 'stroke-categoryInactive'
+						} group-hover:stroke-dashboard`}>
+						<path
+							strokeLinecap="round"
+							strokeLinejoin="round"
+							d="M5.25 14.25h13.5m-13.5 0a3 3 0 01-3-3m3 3a3 3 0 100 6h13.5a3 3 0 100-6m-16.5-3a3 3 0 013-3h13.5a3 3 0 013 3m-19.5 0a4.5 4.5 0 01.9-2.7L5.737 5.1a3.375 3.375 0 012.7-1.35h7.126c1.062 0 2.062.5 2.7 1.35l2.587 3.45a4.5 4.5 0 01.9 2.7m0 0a3 3 0 01-3 3m0 3h.008v.008h-.008v-.008zm0-6h.008v.008h-.008v-.008zm-3 6h.008v.008h-.008v-.008zm0-6h.008v.008h-.008v-.008z"
+						/>
+					</svg>
+					<h2 className="text-categoryInactive w-full text-md ml-2 font-medium hover:text-white">
+						LCTs
 					</h2>
 				</div>
 				<div className="flex items-center py-3 px-6">
