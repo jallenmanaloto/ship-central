0efe6792309d378f0ca28ece14fa303d060000be
jallenmanaloto <84300846+jallenmanaloto@users.noreply.github.com>
2023-09-05 16:00:23 +0800

    Fixed formatting and design of loading report page

--- src/app/dashboard/daily-loading/page.tsx
+++ src/app/dashboard/daily-loading/page.tsx
@@ -13,9 +13,12 @@ const DailyLoading = () => {
 				<h1 className="text-xl text-neutral-800 font-bold py-5 px-4">
 					Daily Loading Report
 				</h1>
-				<div className="grid grid-cols-1 grid-rows-2 gap-4 px-4 pb-4">
+				<div className="grid grid-cols-1 grid-rows-2 md:grid-cols-2 md:grid-rows-none gap-4 px-4 pb-4">
 					<Options projects={projects} />
-					<Button className="py-4 bg-midnight">Search</Button>
+					<div className="grid grid-cols-2 grid-rows-1 md:grid-cols-2 md:grid-rows-none gap-4">
+						<Button className="py-4 bg-midnight opacity-95">Search</Button>
+						<Button className="py-4 bg-sky-950 opacity-75">New record</Button>
+					</div>
 				</div>
 				<div className="pt-10 px-4">
 					<Timeline />
