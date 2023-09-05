1274a5ad35ef8f736f19f6fbb800e4a08861f617
jallenmanaloto <84300846+jallenmanaloto@users.noreply.github.com>
2023-09-05 18:51:06 +0800

    Fixed hover on groups category for mobile

--- src/app/_components/sidebar/MobileNav.tsx
+++ src/app/_components/sidebar/MobileNav.tsx
@@ -40,11 +40,24 @@ const MobileNav = () => {
 						/>
 					</div>
 					<Link href="/dashboard">
-						<div className="flex items-center py-2 px-6">
-							<BiHome
-								size={18}
-								color={`${pathname === '/dashboard' ? '#f4f6f8' : '#919eab'}`}
-							/>
+						<div className="flex items-center py-2 px-6 group">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								fill="none"
+								viewBox="0 0 24 24"
+								stroke-width="1.5"
+								stroke="currentColor"
+								className={`w-5 h-5 ${
+									pathname === '/dashboard'
+										? 'stroke-dashboard'
+										: 'stroke-categoryInactive'
+								} group-hover:stroke-dashboard`}>
+								<path
+									stroke-linecap="round"
+									stroke-linejoin="round"
+									d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+								/>
+							</svg>
 							<h2
 								className={`${
 									pathname === '/dashboard'
@@ -62,13 +75,23 @@ const MobileNav = () => {
 					</div>
 					<Link href="/dashboard/daily-loading">
 						<div className="flex items-center py-3 px-6 cursor-pointer ">
-							<GoStack
-								color={`${
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								fill="none"
+								viewBox="0 0 24 24"
+								strokeWidth={1.5}
+								stroke="currentColor"
+								className={`w-5 h-5 ${
 									pathname === '/dashboard/daily-loading'
-										? '#f4f6f8'
-										: '#919eab'
-								}`}
-							/>
+										? 'stroke-dashboard'
+										: 'stroke-categoryInactive'
+								} group-hover:stroke-dashboard`}>
+								<path
+									strokeLinecap="round"
+									strokeLinejoin="round"
+									d="M6.429 9.75L2.25 12l4.179 2.25m0-4.5l5.571 3 5.571-3m-11.142 0L2.25 7.5 12 2.25l9.75 5.25-4.179 2.25m0 0L21.75 12l-4.179 2.25m0 0l4.179 2.25L12 21.75 2.25 16.5l4.179-2.25m11.142 0l-5.571 3-5.571-3"
+								/>
+							</svg>
 							<h2
 								className={`${
 									pathname === '/dashboard/daily-loading'
