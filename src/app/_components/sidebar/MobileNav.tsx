28f4898f27f2d9e21865d24278605be6a614249f
jallenmanaloto <84300846+jallenmanaloto@users.noreply.github.com>
2023-09-05 21:50:40 +0800

    Fixed highlighting only on active icon

--- src/app/_components/sidebar/MobileNav.tsx
+++ src/app/_components/sidebar/MobileNav.tsx
@@ -137,7 +137,7 @@ const MobileNav = () => {
 							strokeWidth={1.5}
 							stroke="currentColor"
 							className={`w-5 h-5 ${
-								pathname === '/dashboard/daily-loading'
+								pathname === '/dashboard/vessels'
 									? 'stroke-dashboard'
 									: 'stroke-categoryInactive'
 							} group-hover:stroke-dashboard`}>
