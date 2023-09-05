a57c3ffe0602064cece7191877940b2ac5c5a49d
jallenmanaloto <84300846+jallenmanaloto@users.noreply.github.com>
2023-09-05 15:42:54 +0800

    Add hover effet to show options

--- src/app/_components/timeline/index.tsx
+++ src/app/_components/timeline/index.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { AiFillEdit } from 'react-icons/ai'
+import { MdDelete } from 'react-icons/md'
 
 const Timeline = () => {
 	return (
@@ -10,28 +12,36 @@ const Timeline = () => {
 						September 1, 2023
 					</p>
 				</div>
-				<div className="flex my-2 ml-4">
+				<div className="group flex justify-between my-2 ml-4 md:w-3/4 hover:cursor-pointer">
 					<div>
 						<h4 className="mb-1.5 text-base font-medium">1:35PM - 3:20PM</h4>
 						<p className="mb-3 text-neutral-500 dark:text-neutral-300">
 							Vessel arrived port of Surigao pilot station
 						</p>
 					</div>
-					<div className="flex items-center pr-3">
-						<h4>Edit</h4>
-						<h4>Trash</h4>
+					<div className="invisible group-hover:visible flex items-center pr-3 mt-4">
+						<AiFillEdit
+							size={20}
+							color="#454f5b"
+							style={{ marginRight: '4px' }}
+						/>
+						<MdDelete size={20} color="#454f5b" style={{ marginLeft: '4px' }} />
 					</div>
 				</div>
-				<div className="flex my-2 ml-4">
+				<div className="group flex justify-between my-2 ml-4 md:w-3/4 hover:cursor-pointer">
 					<div>
 						<h4 className="mb-1.5 text-base font-medium">3:35PM - 3:45PM</h4>
 						<p className="mb-3 text-neutral-500 dark:text-neutral-300">
 							Resume & cont. loading operation H4&H5
 						</p>
 					</div>
-					<div className="flex items-center pr-3">
-						<h4>Edit</h4>
-						<h4>Trash</h4>
+					<div className="invisible group-hover:visible flex items-center pr-3 mt-4">
+						<AiFillEdit
+							size={20}
+							color="#454f5b"
+							style={{ marginRight: '4px' }}
+						/>
+						<MdDelete size={20} color="#454f5b" style={{ marginLeft: '4px' }} />
 					</div>
 				</div>
 			</li>
@@ -40,16 +50,27 @@ const Timeline = () => {
 				<div className="flex-start flex items-center pt-2">
 					<div className="-ml-[5px] mr-3 h-[9px] w-[9px] rounded-full bg-neutral-900 dark:bg-neutral-500"></div>
 					<p className="text-sm text-neutral-500 dark:text-neutral-300">
-						13.09.2021
+						August 31, 2023
 					</p>
 				</div>
-				<div className="mb-6 ml-4 mt-2">
-					<h4 className="mb-1.5 text-xl font-semibold">Title of section 2</h4>
-					<p className="mb-3 text-neutral-500 dark:text-neutral-300">
-						Libero expedita explicabo eius fugiat quia aspernatur autem
-						laudantium error architecto recusandae natus sapiente sit nam eaque,
-						consectetur porro molestiae ipsam an deleniti.
-					</p>
+				<div className="flex justify-between my-2 ml-4 md:w-3/4">
+					<div>
+						<h4 className="mb-1.5 text-base font-medium">1:35PM - 3:20PM</h4>
+						<p className="mb-3 text-neutral-500 dark:text-neutral-300">
+							Voluptatibus temporibus esse illum eum aspernatur, fugiat suscipit
+							natus! Eum corporis illum nihil officiis tempore. Excepturi illo
+							natus libero sit doloremque, laborum molestias rerum pariatur quam
+							ipsam necessitatibus incidunt, explicabo.
+						</p>
+					</div>
+					<div className="flex items-center pr-3 mt-4">
+						<AiFillEdit
+							size={20}
+							color="#454f5b"
+							style={{ marginRight: '4px' }}
+						/>
+						<MdDelete size={20} color="#454f5b" style={{ marginLeft: '4px' }} />
+					</div>
 				</div>
 			</li>
 
