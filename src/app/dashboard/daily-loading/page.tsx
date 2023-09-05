ac0e0b591f055d306327fd4e8d1a6f5d2454a5c9
jallenmanaloto <84300846+jallenmanaloto@users.noreply.github.com>
2023-09-05 13:17:19 +0800

    Created daily loading page and add shadcn ui

--- src/app/dashboard/daily-loading/page.tsx
+++ src/app/dashboard/daily-loading/page.tsx
@@ -4,7 +4,9 @@ const DailyLoading = () => {
 	return (
 		<div className="flex h-screen max-h-screen w-screen">
 			<div className="w-screen h-screen pb-16 bg-slate-200 overflow-y-scroll">
-				DAILY LOADING PAGE
+				<h1 className="text-xl text-neutral-800 font-bold py-5 px-4">
+					Daily Loading Report
+				</h1>
 			</div>
 		</div>
 	)
