f0481f21ada96224a09fb3f2ec59c8d69af1efa5
jallenmanaloto <84300846+jallenmanaloto@users.noreply.github.com>
2023-09-09 16:01:12 +0800

    Created Lct page and updated nav routing

--- src/app/_components/sidebar/MobileNav.tsx
+++ src/app/_components/sidebar/MobileNav.tsx
@@ -176,28 +176,37 @@ const MobileNav = () => {
 							</h2>
 						</div>
 					</Link>
-					<div className="flex items-center py-3 px-6 cursor-pointer ">
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							fill="none"
-							viewBox="0 0 24 24"
-							strokeWidth={1.5}
-							stroke="currentColor"
-							className={`w-5 h-5 ${
-								pathname === '/dashboard/vessels'
-									? 'stroke-dashboard'
-									: 'stroke-categoryInactive'
-							} group-hover:stroke-dashboard`}>
-							<path
-								strokeLinecap="round"
-								strokeLinejoin="round"
-								d="M5.25 14.25h13.5m-13.5 0a3 3 0 01-3-3m3 3a3 3 0 100 6h13.5a3 3 0 100-6m-16.5-3a3 3 0 013-3h13.5a3 3 0 013 3m-19.5 0a4.5 4.5 0 01.9-2.7L5.737 5.1a3.375 3.375 0 012.7-1.35h7.126c1.062 0 2.062.5 2.7 1.35l2.587 3.45a4.5 4.5 0 01.9 2.7m0 0a3 3 0 01-3 3m0 3h.008v.008h-.008v-.008zm0-6h.008v.008h-.008v-.008zm-3 6h.008v.008h-.008v-.008zm0-6h.008v.008h-.008v-.008z"
-							/>
-						</svg>
-						<h2 className="text-categoryInactive w-full text-md ml-2 font-medium hover:text-white">
-							LCTs
-						</h2>
-					</div>
+					<Link href="/dashboard/lct">
+						<div
+							onClick={toggleDrawer}
+							className="flex items-center py-3 px-6 cursor-pointer ">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								fill="none"
+								viewBox="0 0 24 24"
+								strokeWidth={1.5}
+								stroke="currentColor"
+								className={`w-5 h-5 ${
+									pathname === '/dashboard/lct'
+										? 'stroke-dashboard'
+										: 'stroke-categoryInactive'
+								} group-hover:stroke-dashboard`}>
+								<path
+									strokeLinecap="round"
+									strokeLinejoin="round"
+									d="M5.25 14.25h13.5m-13.5 0a3 3 0 01-3-3m3 3a3 3 0 100 6h13.5a3 3 0 100-6m-16.5-3a3 3 0 013-3h13.5a3 3 0 013 3m-19.5 0a4.5 4.5 0 01.9-2.7L5.737 5.1a3.375 3.375 0 012.7-1.35h7.126c1.062 0 2.062.5 2.7 1.35l2.587 3.45a4.5 4.5 0 01.9 2.7m0 0a3 3 0 01-3 3m0 3h.008v.008h-.008v-.008zm0-6h.008v.008h-.008v-.008zm-3 6h.008v.008h-.008v-.008zm0-6h.008v.008h-.008v-.008z"
+								/>
+							</svg>
+							<h2
+								className={`${
+									pathname === '/dashboard/lct'
+										? 'text-dashboard'
+										: 'text-categoryInactive'
+								} w-full text-md ml-2 font-medium hover:text-white`}>
+								LCTs
+							</h2>
+						</div>
+					</Link>
 					<div className="flex items-center py-3 px-6">
 						<h2 className="text-sm text-category font-semibold tracking-category">
 							FILE & STORAGE
---
+++ src/app/dashboard/lct/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Lct = () => {
+	return (
+		<div className="flex h-screen max-h-screen md:ml-[240px]">
+			<div className="w-screen h-screen pb-56 px-6 bg-slate-200 overflow-y-scroll">
+				This is the LCT Page
+			</div>
+		</div>
+	)
+}
+
+export default Lct
