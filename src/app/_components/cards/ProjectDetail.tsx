7e6f3a35e6f7c923de0571209e8603c8e2d9f5cd
jallenmanaloto <84300846+jallenmanaloto@users.noreply.github.com>
2023-09-09 11:56:44 +0800

    Add switch on card

--- src/app/_components/cards/ProjectDetail.tsx
+++ src/app/_components/cards/ProjectDetail.tsx
@@ -2,13 +2,17 @@ import { ProjDetail } from '@/utils/types'
 import React from 'react'
 import dayjs from 'dayjs'
 import MoreDetails from '../accordion/my-projects/MoreDetails'
+import { Switch } from '@/components/ui/switch'
 
 const ProjectDetail = ({ project }: { project: ProjDetail }) => {
 	return (
 		<div className="block rounded-lg bg-white p-6 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)">
-			<h2 className="text-base font-semibold text-neutral-800">
-				{project.name}
-			</h2>
+			<div className="flex justify-between">
+				<h2 className="text-base font-semibold text-neutral-800">
+					{project.name}
+				</h2>
+				<Switch />
+			</div>
 			<h4 className="text-sm text-neutral-800">
 				{`(${dayjs(project.projectStartDate).format('MMM DD, YYYY')} - ${dayjs(
 					project.projectEndDate
