8cbfa71adebdee6757f68e4e1264cbfccdeb9fbb
jallenmanaloto <84300846+jallenmanaloto@users.noreply.github.com>
2023-09-09 11:46:49 +0800

    Add details on accordion content

--- src/app/_components/accordion/my-projects/MoreDetails.tsx
+++ src/app/_components/accordion/my-projects/MoreDetails.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import React, { useState } from 'react'
+import vesselAnalytics from '@/app/faker/data/analytics'
 import {
 	Accordion,
 	AccordionContent,
@@ -8,14 +9,61 @@ import {
 	AccordionTrigger,
 } from '@/components/ui/accordion'
 
+const Content = () => {
+	return (
+		<>
+			<div className="flex w-full">
+				<h3 className="text-neutral-800 font-medium">Laytime:</h3>
+				<h3 className="text-neutral-800 px-2">6.78</h3>
+			</div>
+			<div className="flex w-full">
+				<h3 className="text-neutral-800 font-medium">Laytime as of:</h3>
+				<h3 className="text-neutral-800 px-2">0.48</h3>
+			</div>
+			<div className="flex w-full">
+				<h3 className="text-neutral-800 font-medium">Laytime consumed:</h3>
+				<h3 className="text-neutral-800 px-2">8.04</h3>
+			</div>
+			<div className="flex w-full">
+				<h3 className="text-neutral-800 font-medium">Total cargo:</h3>
+				<h3 className="text-neutral-800 px-2">60,500</h3>
+			</div>
+			<div className="flex w-full">
+				<h3 className="text-neutral-800 font-medium">Total cargo loaded:</h3>
+				<h3 className="text-neutral-800 px-2">27,613.24</h3>
+			</div>
+			<div className="flex w-full">
+				<h3 className="text-neutral-800 font-medium">
+					Total cargo to be loaded:
+				</h3>
+				<h3 className="text-neutral-800 px-2">32,886.76</h3>
+			</div>
+			<div className="flex w-full">
+				<h3 className="text-neutral-800 font-medium">Est. dispatch:</h3>
+				<h3 className="text-neutral-800 px-2">9.09</h3>
+			</div>
+			<div className="flex w-full">
+				<h3 className="text-neutral-800 font-medium">
+					Est. time to complete loading:
+				</h3>
+				<h3 className="text-neutral-800 px-2">9.58</h3>
+			</div>
+			<div className="flex w-full">
+				<h3 className="text-neutral-800 font-medium">Est. time to finish:</h3>
+				<h3 className="text-neutral-800 px-2">9.58</h3>
+			</div>
+		</>
+	)
+}
+
 const MoreDetails = () => {
 	return (
 		<>
 			<Accordion type="single" collapsible>
 				<AccordionItem value="item-1">
-					<AccordionTrigger>Is it accessible?</AccordionTrigger>
+					<AccordionTrigger>More details</AccordionTrigger>
 					<AccordionContent>
-						Yes. It adheres to the WAI-ARIA design pattern.
+						<Content />
 					</AccordionContent>
 				</AccordionItem>
 			</Accordion>
--- src/app/dashboard/projects/page.tsx
+++ src/app/dashboard/projects/page.tsx
@@ -14,15 +14,17 @@ const MyProjects = () => {
 				<h1 className="text-xl text-neutral-800 font-bold py-5 px-4">
 					My Projects
 				</h1>
-				<div className="grid grid-cols-1 grid-rows-2 md:grid-cols-2 md:grid-rows-none gap-4 px-4 pb-4">
+				<div className="grid grid-cols-1 grid-rows-2 md:grid-cols-3 md:grid-rows-none gap-4 px-4 pb-4">
 					<Projects projects={projects} option="projectName" />
 					<Projects projects={projects} option="projectDate" />
+					<div className="grid grid-cols-1 grid-rows-1 md:grid-cols-1 md:grid-rows-none gap-4">
+						<div className="grid grid-cols-2 gap-4">
+							<Button className="py-4 bg-midnight opacity-95">Search</Button>
+							<CreateUpdate action="create" />
+						</div>
+					</div>
 				</div>
-				<div className="grid grid-cols-2 grid-rows-1 md:grid-cols-2 md:grid-rows-none gap-4 px-4">
-					<Button className="py-4 bg-midnight opacity-95">Search</Button>
-					<CreateUpdate action="create" />
-				</div>
-				<div className="mt-12">
+				<div className="mt-12 grid grid-cols-1 md:grid-cols-3 gap-4">
 					{projects.map((project, idx) => {
 						return (
 							<div key={idx} className="py-3 px-4">
