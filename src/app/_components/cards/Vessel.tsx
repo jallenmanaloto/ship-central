c189d616bc3d9d4f064ea1422f8d44f9a2c02dac
jallenmanaloto <84300846+jallenmanaloto@users.noreply.github.com>
2023-09-25 19:09:35 +0800

    removed unused import

--- src/app/_components/cards/Vessel.tsx
+++ src/app/_components/cards/Vessel.tsx
@@ -7,7 +7,6 @@ import Modal from '@mui/joy/Modal'
 import ModalClose from '@mui/joy/ModalClose'
 import Typography from '@mui/joy/Typography'
 import Sheet from '@mui/joy/Sheet'
-import { Input } from '@/components/ui/input'
 import { ReloadIcon } from '@radix-ui/react-icons'
 
 const ConfirmDelete = ({ vesselId }: { vesselId: string }) => {
@@ -73,7 +72,7 @@ const ConfirmDelete = ({ vesselId }: { vesselId: string }) => {
 						mb={1}>
 						Delete confirmation
 					</Typography>
-					<h2 className="py-3">Are you sure you want to delete this vessel?</h2>
+					<h2 className="py-3">Are you sure you want to delete this Vessel?</h2>
 
 					<div className="grid grid-cols-2 gap-3 py-3">
 						<Button onClick={handleDelete} className="bg-sky-950 opacity-75">
@@ -102,19 +101,6 @@ const Vessel = ({ vessel }: { vessel: TVessel }) => {
 			className="block group rounded-lg bg-white p-6 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)">
 			<div className="relative">
 				<CreateUpdate action="update" vessel={vessel} />
-				{/* <svg
-					xmlns="http://www.w3.org/2000/svg"
-					fill="none"
-					viewBox="0 0 24 24"
-					strokeWidth={1.5}
-					stroke="currentColor"
-					className="absolute right-0 w-5 h-5 stroke-slate-600 invisible group-hover:visible cursor-pointer">
-					<path
-						strokeLinecap="round"
-						strokeLinejoin="round"
-						d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
-					/>
-				</svg> */}
 				<ConfirmDelete vesselId={vessel.id} />
 				<h2 className="font-semibold text-sm text-neutral-800">
 					{vessel.name}
